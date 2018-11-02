277fdefe72007e94e212c2c1a609e3c61d005648
ZujuSun <sunzuju@gmail.com>
2018-11-02 17:07:36 -0400

    add more stuff

---
+++ JS-Sneaker/Application/Component/SpotlightTableViewCell.swift
@@ -0,0 +1,10 @@
+//
+//  SpotlightTableViewCell.swift
+//  JS-Sneaker
+//
+//  Created by Jackie Sun on 2018-11-02.
+//  Copyright © 2018 Jackie Sun. All rights reserved.
+//
+
+import UIKit
+
---
+++ JS-Sneaker/Application/UserInterface/MainViewController/MainViewController.swift
@@ -0,0 +1,64 @@
+//
+//  MainViewController.swift
+//  JS-Sneaker
+//
+//  Created by Jackie Sun on 2018-11-02.
+//  Copyright © 2018 Jackie Sun. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    let tableView: UITableView
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        self.tableView = UITableView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.blue
+        self.navigationController?.navigationBar.barTintColor = UIColor.black
+        
+        self.title = "Home"
+        
+        self.setupTableView()
+    }
+    
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+}
+
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 450.0;
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = 
+    }
+}
