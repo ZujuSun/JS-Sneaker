48a1d2bfc6c4eafd6fc1f385ca94c911d8172f05
ZujuSun <sunzuju@gmail.com>
2018-11-02 13:46:56 -0400

    initial setup

---
+++ JS-Sneaker/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  JS-Sneaker
+//
+//  Created by Jackie Sun on 2018-11-02.
+//  Copyright © 2018 Jackie Sun. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.title = "hehe"
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
---
+++ JS-Sneaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JS-Sneaker
+//
+//  Created by Jackie Sun on 2018-11-02.
+//  Copyright © 2018 Jackie Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
